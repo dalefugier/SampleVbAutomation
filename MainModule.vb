ac88e1143629d9a54d92fc8a45d80b5fb0e277ad
dalefugier <dale@mcneel.com>
2015-04-08 08:44:07 -0700

    Initial commit

---
+++ MainModule.vb
@@ -0,0 +1,62 @@
+﻿Imports System
+Imports System.Threading
+Imports Rhino4
+Imports RhinoScript4
+
+Module MainModule
+
+  Sub Main()
+
+    'Const rhinoId = "Rhino5.Application";
+    Const rhinoId As String = "Rhino5.Interface"
+    'Const rhinoId As String = "Rhino5x64.Application"
+    'Const rhinoId = "Rhino5x64.Interface";
+
+    'Dim rhinoObj As Rhino5Application = Nothing
+    Dim rhinoObj As Rhino5Interface = Nothing
+
+    Try
+      Dim rhinoType As Type = Type.GetTypeFromProgID(rhinoId)
+      rhinoObj = Activator.CreateInstance(rhinoType)
+    Catch
+    End Try
+
+    If (rhinoObj Is Nothing) Then
+      Console.WriteLine("Failed to create: " & rhinoId)
+      Return
+    End If
+
+    Const bailTime As Integer = 15 * 1000
+    Dim waitTime As Integer = 0
+    While (0 = rhinoObj.IsInitialized())
+      Thread.Sleep(100)
+      waitTime += 100
+      If (waitTime > bailTime) Then
+        Console.WriteLine("Rhino initialization failed")
+        Return
+      End If
+    End While
+
+    rhinoObj.Visible = 1
+
+    Dim rsObj As RhinoScript = rhinoObj.GetScriptObject()
+    If (rsObj Is Nothing) Then
+      Console.WriteLine("Failed to create RhinoScript object")
+      Return
+    End If
+
+    Dim strObj = rsObj.GetObject("Select a surface", 8)
+    If (Not IsDBNull(strObj)) Then
+      Console.WriteLine("ID: " & strObj)
+
+      Dim arrMassProp = rsObj.SurfaceAreaMoments(strObj)
+      If (Not IsDBNull(arrMassProp)) Then
+        Dim str = rsObj.Pt2Str(arrMassProp(6), 4)
+        Console.WriteLine("Area Moments of Inertia about the World Coordinate Axes: " & str)
+      End If
+
+    End If
+
+  End Sub
+
+End Module
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# SampleVbAutomation
+SampleVbAutomation
+====================
+
 Demonstrates how to automate Rhino 5, via COM, from a standalone VB.NET application.
+
+Building Sample
+--------------------
+To build the sample, you are going to need:
+
+* Rhinoceros 5 (http://www.rhino3d.com)
+* Microsoft Visual Basic 2010
+
+Note, project requires that you create Interop assemblies by adding references to the
+Rhino type library (Rhino5.tlb) and the RhinoScript type library (RhinoScript.tlb).
+
+Legal Stuff
+-----------
+Copyright © 2015 Robert McNeel & Associates. All Rights Reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+Software.
+
+THIS SOFTWARE IS PROVIDED "AS IS" WITHOUT EXPRESS OR IMPLIED WARRANTY. ALL IMPLIED
+WARRANTIES OF FITNESS FOR ANY PARTICULAR PURPOSE AND OF MERCHANTABILITY ARE HEREBY
+DISCLAIMED.
+
+Rhinoceros is a registered trademark of Robert McNeel & Associates.
